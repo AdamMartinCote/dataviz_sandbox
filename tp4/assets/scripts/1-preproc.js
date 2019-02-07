389ccc2b450957920bb434f365320f7a30a420c0
Adam Martin-Côté <adammartincote@gmail.com>
2019-02-07 06:52:32 -0500

    mandatory formatting commit

--- tp4/assets/scripts/1-preproc.js
+++ tp4/assets/scripts/1-preproc.js
@@ -6,13 +6,15 @@
 
 
 /**
- * Précise le domaine de l'échelle de couleurs qui est utilisée pour distinguer chacune des stations de BIXI.
+ * Précise le domaine de l'échelle de couleurs qui est utilisée pour
+ * distinguer chacune des stations de BIXI.
  *
  * @param color   Échelle de couleurs.
  * @param data    Données provenant du fichier JSON.
  */
 function domainColor(color, data) {
-  // TODO: Préciser le domaine de l'échelle de couleurs en y associant les stations de BIXI utilisées.
+  // TODO: Préciser le domaine de l'échelle de couleurs en y associant
+  // les stations de BIXI utilisées.
 
 }
 
@@ -23,7 +25,8 @@ function domainColor(color, data) {
  * @param data    Données provenant du fichier JSON.
  */
 function domainX(x, data) {
-  // TODO: Préciser le domaine pour la variable "x" en y associant les stations de BIXI utilisées.
+  // TODO: Préciser le domaine pour la variable "x" en y associant les
+  // stations de BIXI utilisées.
 
 }
 
@@ -34,18 +37,22 @@ function domainX(x, data) {
  * @param currentData   Les données qui sont actuellement utilisées par le diagramme.
  */
 function domainY(y, currentData) {
-  // TODO: Préciser le domaine pour la variable "y" en prenant comme minimum et maximum le nombre de trajets vers une station de BIXI.
+  // TODO: Préciser le domaine pour la variable "y" en prenant comme
+  // minimum et maximum le nombre de trajets vers une station de BIXI.
 
 }
 
 /**
- * Obtient la matrice d'adjacence à partir des données spécifiées pour créer le diagramme à cordes.
+ * Obtient la matrice d'adjacence à partir des données spécifiées pour
+ * créer le diagramme à cordes.
  *
  * @param data        Données provenant du fichier JSON.
- * @return {Array}    Une matrice de 10 x 10 indiquant le nombre de trajets partant et se dirigeant vers une station précise.
+ * @return {Array} 		Une matrice de 10 x 10 indiquant le nombre de
+ * 										trajets partant et se dirigeant vers une station précise.
  */
 function getMatrix(data) {
-  // TODO: Calculer la matrice d'adjacence pour créer le diagramme à cordes.
+  // TODO: Calculer la matrice d'adjacence pour créer le diagramme à
+  // cordes.
   return [];
 }
 
@@ -55,6 +62,7 @@ function getMatrix(data) {
  * @param data    Données provenant du fichier JSON.
  */
 function getTotal(data) {
-  // TODO: Calculer le nombre total de trajets réalisés pour le mois d'août 2015.
+  // TODO:	Calculer le nombre total de trajets réalisés pour le mois
+  // 				d'août 2015.
   return 0;
 }
--- tp4/assets/scripts/2-bar-chart.js
+++ tp4/assets/scripts/2-bar-chart.js
@@ -8,36 +8,41 @@
 /**
  * Crée les axes du graphique à bandes.
  *
- * @param g       Le groupe SVG dans lequel le graphique à bandes doit être dessiné.
+ * @param g 			Le groupe SVG dans lequel le graphique à bandes doit être
+ * 								dessiné.
  * @param xAxis   L'axe X.
  * @param yAxis   L'axe Y.
  * @param height  La hauteur du graphique.
  */
 function createAxes(g, xAxis, yAxis, height) {
-  // TODO: Dessiner les axes X et Y du graphique. Assurez-vous d'indiquer un titre pour l'axe Y.
-  // Axe horizontal
+  // TODO: Dessiner les axes X et Y du graphique. Assurez-vous
+  // d'indiquer un titre pour l'axe Y.  Axe horizontal
 
 }
 
 /**
  * Crée le graphique à bandes.
  *
- * @param g             Le groupe SVG dans lequel le graphique à bandes doit être dessiné.
+ * @param g 						Le groupe SVG dans lequel le graphique à bandes doit être
+ * 											dessiné.
  * @param currentData   Les données à utiliser.
  * @param x             L'échelle pour l'axe X.
  * @param y             L'échelle pour l'axe Y.
- * @param color         L'échelle de couleurs qui est associée à chacun des noms des stations de BIXI.
+ * @param color 				L'échelle de couleurs qui est associée à chacun des
+ * 											noms des stations de BIXI.
  * @param tip           L'infobulle à afficher lorsqu'une barre est survolée.
  * @param height        La hauteur du graphique.
  */
 function createBarChart(g, currentData, x, y, color, tip, height) {
-  // TODO: Dessiner les cercles à bandes en utilisant les échelles spécifiées.
-  //       Assurez-vous d'afficher l'infobulle spécifiée lorsqu'une barre est survolée.
+  // TODO: Dessiner les cercles à bandes en utilisant les échelles
+  //       spécifiées.  Assurez-vous d'afficher l'infobulle spécifiée
+  //       lorsqu'une barre est survolée.
 
 }
 
 /**
- * Réalise une transition entre les données actuellement utilisées et les nouvelles qui doivent être utilisées.
+ * Réalise une transition entre les données actuellement utilisées et
+ * les nouvelles qui doivent être utilisées.
  *
  * @param g         Le groupe SVG dans lequel le graphique à bandes est dessiné.
  * @param newData   Les nouvelles données à utiliser.
@@ -47,8 +52,9 @@ function createBarChart(g, currentData, x, y, color, tip, height) {
  */
 function transition(g, newData, y, yAxis, height) {
   /* TODO:
-   - Réaliser une transition pour mettre à jour l'axe des Y et la hauteur des barres à partir des nouvelles données.
-   - La transition doit se faire en 1 seconde.
+		- Réaliser une transition pour mettre à jour l'axe des Y et
+   		la hauteur des barres à partir des nouvelles données.
+		- La transition doit se faire en 1 seconde.
    */
 
 }
@@ -62,8 +68,9 @@ function transition(g, newData, y, yAxis, height) {
  * @return {string}       Le texte à afficher dans l'infobulle.
  */
 function getToolTipText(d, currentData, formatPercent) {
-  // TODO: Retourner le texte à afficher dans l'infobulle selon le format demandé.
-  //       Assurez-vous d'utiliser la fonction "formatPercent" pour formater le pourcentage correctement.
+  // TODO: Retourner le texte à afficher dans l'infobulle selon le
+  //       format demandé.  Assurez-vous d'utiliser la fonction
+  //       "formatPercent" pour formater le pourcentage correctement.
 
   return "";
 }
