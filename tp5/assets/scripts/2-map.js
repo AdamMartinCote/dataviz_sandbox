0884ac9a577c1fa2b5b9cb34511ae20bbb2dc879
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-08 07:42:05 -0600

    update district; colors are wrong

--- tp5/assets/scripts/2-map.js
+++ tp5/assets/scripts/2-map.js
@@ -104,4 +104,16 @@ function createDistricts(g, path, canada, sources, color, showPanel) {
 function updateMap(svg, g, path, canada) {
   // TODO: Mettre à jour l'élément SVG, la position du groupe "g" et l'affichage des tracés en vous basant sur l'exemple fourni.
 
+  let bounds = path.bounds(canada); // ???
+  console.log(bounds);
+  let topLeft = bounds[0],
+      bottomRight = bounds[1];
+  svg
+    .attr("width",  bottomRight[0] - topLeft[0])
+    .attr("height", bottomRight[1] - topLeft[1])
+    .style("left", topLeft[0] + "px")
+    .style("top",  topLeft[1] + "px");
+  g
+    .attr("transform", "translate(" + -topLeft[0] + ","
+	  + -topLeft[1] + ")");
 }
