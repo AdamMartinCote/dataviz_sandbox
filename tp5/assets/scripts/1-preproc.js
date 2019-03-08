db32699a6d43ae69452af0b61ef047c29438e7eb
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-08 07:28:10 -0600

    added color scale

--- tp5/assets/scripts/1-preproc.js
+++ tp5/assets/scripts/1-preproc.js
@@ -15,7 +15,8 @@ function colorScale(color, parties) {
   // TODO: Préciser le domaine de l'échelle en y associant chacun des partis politique de la liste spécifiée en paramètre.
   //       De plus, préciser la gamme de couleurs en spécifiant les couleurs utilisées par chacun des partis.
     color.domain(parties.map(d => d.name));
-    // console.log(parties);
+    color.range(parties.map(d => d.color));
+    // console.log(parties)
 }
 
 /**
